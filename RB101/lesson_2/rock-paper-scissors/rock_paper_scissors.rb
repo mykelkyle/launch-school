33c1505b3611e819c0fae4f9e5f7c7c81d14ded5
Mykel Murphy <murphy@akane.waseda.jp>
2023-02-21 08:05:12 -0500

    Add rock paper scissors walkthrough

---
+++ RB101/lesson_2/rock-paper-scissors/rock_paper_scissors.rb
@@ -0,0 +1,45 @@
+VALID_CHOICES = ["rock", "paper", "scissors"]
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def display_results(player, computer)
+  if (player == "rock" && computer == "scissors") ||
+     (player == "paper" && computer == "rock") ||
+     (player == "scissors" && computer == "paper")
+    prompt("You won!")
+  elsif (player == "rock" && computer == "paper") ||
+        (player == "paper" && computer == " scissors") ||
+        (player == "scissors" && computer == "rock")
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do
+  choice = ""
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("Invalid choice. Try again.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You chose #{choice}; Computer chose: #{computer_choice}")
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again? (y) for yes")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?("y")
+end
+
+prompt("Thank you for playing. Goodbye!")
