c9ade67f31a3f45612084d2fafa4e44233395462
Mykel Murphy <murphy@akane.waseda.jp>
2023-08-09 13:35:47 -0400

    complete exercise set

---
+++ RB120/OOP_exercises/debugging/5.rb
@@ -0,0 +1,48 @@
+class File
+  attr_accessor :name, :byte_content
+
+  def initialize(name)
+    @name = name
+  end
+
+  alias_method :read,  :byte_content
+  alias_method :write, :byte_content=
+
+  def copy(target_file_name)
+    target_file = self.class.new(target_file_name)
+    target_file.write(read)
+
+    target_file
+  end
+
+  def to_s
+    "#{name}.#{@format}"
+  end
+end
+
+class MarkdownFile < File
+  def initialize(name)
+    super(name)
+    @format = :md
+  end
+end
+
+class VectorGraphicsFile < File
+  @format = :svg
+end
+
+class MP3File < File
+  @format = :mp3
+end
+
+# Test
+
+blog_post = MarkdownFile.new('Adventures_in_OOP_Land')
+blog_post.write('Content will be added soon!'.bytes)
+
+copy_of_blog_post = blog_post.copy('Same_Adventures_in_OOP_Land')
+
+puts copy_of_blog_post.is_a? MarkdownFile     # true
+puts copy_of_blog_post.read == blog_post.read # true
+
+puts blog_post
