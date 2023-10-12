a973642f960bf50ded1ad709bd721bc75a67159d
Mykel Murphy <murphy@akane.waseda.jp>
2023-10-12 11:13:31 -0400

    add practice files

--- RB130/lesson_1/test.rb
+++ RB130/lesson_1/test.rb
@@ -1,12 +1,7 @@
-def each(arr)
-  counter = 0
-  while counter < arr.size
-    yield(arr[counter])
-    counter += 1
-  end
-
-  arr
+def welcome(str)
+  p block_given?
 end
 
-array = [1, 2, 3]
-each([1, 2, 3, 4, 5]) {|num| "do nothing"}.select{ |num| puts num if num.odd? }     # => [1, 3, 5]
+welcome('Hello') { 'there' } # => Hello there
+welcome('Hey') { 'Joe' }   # => Hey Joe
+welcome('Hi!')                # => Hi! (there's a space after the !)
